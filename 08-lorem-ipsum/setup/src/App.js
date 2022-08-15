136c3953227ec6cee56918f37169bf6c93c0a3c2
usman <kohistani5525@gmail.com>
2022-08-16 01:03:00 +0500

    showModal implimented

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,34 +1,41 @@
-import React, { useState, useEffect } from 'react';
+//even the type of input is number we only get string in
+//minimum value for paragraph can only be 1 and max is 8
+
+import React, { useState } from 'react';
 import Modal from './Modal';
 import data from './data';
 function App() {
   const [count, setCount] = useState('');
-  const [render, setRender] = useState(false);
+  const [showModal, setshowModal] = useState(false);
   const [text, setText] = useState([]);
 
   let amount = +count || 1;
 
-  useEffect(() => {
-    setTimeout(() => {
-      renderModal();
-    }, 1000);
-  }, [amount]);
-
-  const renderModal = () => {
-    setRender(!render);
-  };
+  //after 2s set showModal to false
+  const showmodal = setTimeout(() => {
+    setshowModal(false);
+  }, 2000);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    //max paragraph can only be 8
     if (count > 8) amount = 8;
+    //slicing the arr based on the amount(count) got from the user
     setText(data.slice(0, amount));
-    setRender(true);
+    //modal is showing
+    setshowModal(true);
+    //to hide th modal after 2s
+    showmodal();
   };
-  console.log(count);
-  const handleChange = (e) => setCount(e.target.value);
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+
   return (
     <>
-      {render && <Modal count={amount} />}
+      {/* showModal is true the render Moadl */}
+      {showModal && <Modal count={amount} />}
 
       <div className='section-center'>
         <h2>tired of boring lorem Ipsum?</h2>
