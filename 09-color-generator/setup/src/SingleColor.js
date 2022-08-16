a0d28aeec94dcf973a466bc74b7c022555c4c74b
usman <kohistani5525@gmail.com>
2022-08-17 01:56:09 +0500

    9-color-Generator complete

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+  const hex = rgbToHex(...rgb);
 
-export default SingleColor
+  const copyColorCode = () => {
+    navigator.clipboard.writeText(hex);
+    setAlert(true);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${rgb})` }}
+      onClick={copyColorCode}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hex}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
